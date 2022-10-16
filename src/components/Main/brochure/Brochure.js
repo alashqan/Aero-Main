e53f517355afc19a2dbf1855802be5223336a15e
Al Ashqan Babu <80443704+alashqan@users.noreply.github.com>
2022-10-16 08:30:07 +0530

    Create Brochure.js

---
+++ src/components/Main/brochure/Brochure.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Brochure.css'
+
+export default function Brochure() {
+  return (
+    <div className='brochure_div'>
+      <h2>Download Rulebook</h2>
+      <h3>Details and Schedule of Aerophilia 2022</h3>
+      <button className='button-63'>Rulebook</button>
+    </div>
+  )
+}
