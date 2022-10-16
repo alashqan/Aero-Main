8bdc62a4949d05a192b2907be45f164c85fd0bf6
Al Ashqan Babu <80443704+alashqan@users.noreply.github.com>
2022-10-16 08:31:51 +0530

    Initial Commit

---
+++ src/components/Navbar/NavItems.js
@@ -0,0 +1,32 @@
+export const navItems = [
+    {
+        id:1,
+        title:"HOME",
+        path:"/",
+        cName:'nav-item'
+    },
+    {
+        id:2,
+        title:"EVENTS",
+        path:"./events",
+        cName:'nav-item'
+    },
+    // {
+    //     id:3,
+    //     title:"SPONSORS",
+    //     path:"./",
+    //     cName:'nav-item'
+    // },
+    {
+        id:3,
+        title:"GALLERY",
+        path:"./gallery",
+        cName:'nav-item'
+    },
+    // {
+    //     id:4,
+    //     title:"ABOUT US",
+    //     path:"./aboutus",
+    //     cName:'nav-item'
+    // },
+]
